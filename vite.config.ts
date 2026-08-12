e01753963ba6ea0ca62d128e389be8f8251d2517
suhailarfe <167489221+suhailarfe@users.noreply.github.com>
2026-08-12 09:13:54 +0300

    🔧 إعدادات النشر + تنظيف الملفات القديمة

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,3 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: { 'react-is': 'react-is' }
-  }
-})
+export default defineConfig({ plugins: [react()] })
